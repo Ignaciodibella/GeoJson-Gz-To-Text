e41bf617b4cf70956873feebb0b557830dbde1ed
Ignaciodibella <69056299+Ignaciodibella@users.noreply.github.com>
2024-08-05 09:53:57 -0300

    Update main.py
    Se cambia la impresión del resultado por la exportación en txt

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ file_path = 'OSMB-2da73f57a5986989e2aaf53adc5b371380858757.geojson.gz'
 with gzip.open(file_path, 'rt', encoding='utf-8') as f:
     geojson_data = geojson.load(f)
 
-# Mostrar Data
-print(geojson_data)
+with open('output.txt', 'w') as f:
+    geojson.dump(geojson_data, f)
+
+print("Exportación Finalizada")
+
